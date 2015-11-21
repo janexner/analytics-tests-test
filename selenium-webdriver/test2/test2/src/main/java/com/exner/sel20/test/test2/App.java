d4f7a370f970115de523870719c1508e5b0c4caa
Jan Exner <jexner@adobe.com>
2015-11-21 11:58:51 +0100

    testing with maven and selenium webdriver

---
+++ selenium-webdriver/test2/test2/src/main/java/com/exner/sel20/test/test2/App.java
@@ -0,0 +1,13 @@
+package com.exner.sel20.test.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
